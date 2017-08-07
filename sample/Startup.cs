a0bc4b0c7d06d7db9767ece44421c634fe208bc6
nohjee <supernohjee@gmail.com>
2017-08-07 19:23:01 +0900

    - initial commit2

---
+++ sample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sample.Startup))]
+namespace sample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
