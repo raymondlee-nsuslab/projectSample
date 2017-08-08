d6221551b648a72725e69680f402a6c7aa1a69cd
nohjee <supernohjee@gmail.com>
2017-08-08 11:29:20 +0900

    - added files

---
+++ DTO/Main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace DTO
+{
+    public class Main
+	{
+		[Route("/player/checkbonus")]
+		public class CheckBonusRequest
+		{
+			public string Brand { get; set; }
+			public string BonusCode { get; set; }
+		}
+	}
+}
---
+++ Sample.Main/SampleMain.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace Sample.Main
+{
+	public class SampleMain
+	{
+		public async Task<string> Get(DTO.Main.CheckBonusRequest request)
+		{
+			return "BonusCode not specified.";
+		}
+	}
+}
