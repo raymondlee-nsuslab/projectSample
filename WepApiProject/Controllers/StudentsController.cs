86842d30f3ec6e49357fee1495dcdb9c9c4708f3
raymond.lee <raymond.lee@nsuslab.com>
2017-09-08 17:58:43 +0900

    * WebAPI 프로젝트 추가입니다.

---
+++ WepApiProject/Controllers/StudentsController.cs
@@ -0,0 +1,61 @@
+﻿using System.Net;
+using System.Web.Mvc;
+using StudentData;
+
+namespace WepApiProject.Controllers
+{
+    public class StudentsController : Controller
+    {
+        [HttpGet]
+        public ActionResult GetTitles()
+        {
+            var studentManage = new StudentManage();
+            var titles = studentManage.GetTitleList();
+            return Json(titles , JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult GetSchools(SchoolListRequest request)
+        {
+            var studentManage = new StudentManage();
+            var schools = studentManage.GetSchoolList(request);
+            return Json(new
+            {
+                draw = request.Draw,
+                recordsFiltered = schools.TotalRecord,
+                recordTotal = schools.TotalRecord,
+                data = schools.SchoolLists
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult GetStudent(int enrollId)
+        {
+            var studentManage = new StudentManage();
+            
+            return Json(studentManage.GetStudent(enrollId),JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPut]
+        public ActionResult UpdateStudent(Student student)
+        {
+            var status = false;
+            if (ModelState.IsValid)
+            {
+                var studentManage = new StudentManage();
+                status = studentManage.SetStudent(student);
+            }
+            return Json(status);
+        }
+
+        [HttpDelete]
+        public ActionResult DeleteStudent(Student student)
+        {
+            var status = false;
+            var studentManage = new StudentManage();
+            status = studentManage.DeleteStudent(student);
+            return Json(status);
+        }
+    }
+
+}
